5fc1570c9d883ae3d64915fd897fb8446d3240f9
hema <hemanaraharisetti@gmail.com>
2025-06-29 22:27:48 +0530

    fix: updated page.tsx and tailwind config

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white bg-gray-900">
+      <h1 className="text-4xl font-bold">Welcome to My Portfolio 👋</h1>
+    </div>
+  );
+}
